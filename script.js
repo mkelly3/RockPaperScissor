ea11723147bfb999fa654a63be5e24aabd11e537
Angela-Amaro <aamaro@alumnae.mills.edu>
2023-02-20 16:58:41 -0600

    added a error message if the player does not select R,P or S on their keyboard

--- script.js
+++ script.js
@@ -1,49 +1,46 @@
-var tie=0;
-var win=0;
-var loss=0;
-var RPS=["R", "P", "S"];
-//math floor and random is randomly selecting a value within var RPS
-var compPick= RPS[Math.floor(Math.random()*RPS.length)];
+var tie = 0;
+var win = 0;
+var loss = 0;
+var RPS = ["R", "P", "S"];
+//math random is randomly selecting a value within var RPS
+//math floor rounds the number to a whole number to pick from the array
+var compPick = RPS[Math.floor(Math.random() * RPS.length)];
 console.log(compPick);
 
-function playGame(){
-   var playerPick=prompt("Please pick R, P, or S");
-   if (playerPick===compPick){
-    tie++
+function playGame() {
+  var playerPick = prompt("Please pick R, P, or S");
+  if (playerPick === compPick) {
+    tie++;
     alert("It's a tie! ties: " + tie + " wins: " + win + " losses: " + loss);
     playGame();
-   }
-   else if (playerPick=="R"&& compPick=="P"){
-    loss++
+  } else if (playerPick == "R" && compPick == "P") {
+    loss++;
     alert("You lose :,( ties: " + tie + " wins: " + win + " losses: " + loss);
     playGame();
-   }
-   else if (playerPick=="R"&& compPick=="S"){
+  } else if (playerPick == "R" && compPick == "S") {
     win++;
     confirm("You win! ties: " + tie + " wins: " + win + " losses: " + loss);
     playGame();
-   }
-   else if (playerPick=="S"&& compPick=="R"){
-    loss++
+  } else if (playerPick == "S" && compPick == "R") {
+    loss++;
     alert("You lose :,( ties: " + tie + " wins: " + win + " losses: " + loss);
     playGame();
-   }
-   else if (playerPick=="S"&& compPick=="P"){
+  } else if (playerPick == "S" && compPick == "P") {
     win++;
     confirm("You win! ties: " + tie + " wins: " + win + " losses: " + loss);
     playGame();
-   }
-   else if (playerPick=="P"&& compPick=="S"){
-    loss++
+  } else if (playerPick == "P" && compPick == "S") {
+    loss++;
     alert("You lose :,( ties: " + tie + " wins: " + win + " losses: " + loss);
     playGame();
-   }
-   else if (playerPick=="P"&& compPick=="R"){
+  } else if (playerPick == "P" && compPick == "R") {
     win++;
     confirm("You win! ties: " + tie + " wins: " + win + " losses: " + loss);
     playGame();
-   }
+  } else {
+    playerPick != RPS;
+    alert("Please pick R, P, or S only");
+  }
 }
 
 playGame();
-
